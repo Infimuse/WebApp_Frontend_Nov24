3077dc333fb427aa41b7e4949480d489a70f886d
Victor <victor.owiti@zeraki.app>
2025-02-08 21:52:01 +0300

    changes yml1

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
-      domains: ['links.papareact.com', 'images.unsplash.com','randomuser.me', "www.pexels.com","infimuse.s3.amazonaws.com", "images.pexels.com","drive.google.com"],
-    },
+    domains: [
+      "links.papareact.com",
+      "images.unsplash.com",
+      "randomuser.me",
+      "www.pexels.com",
+      "infimuse.s3.amazonaws.com",
+      "images.pexels.com",
+      "drive.google.com",
+    ],
+  },
 };
 
 export default nextConfig;
